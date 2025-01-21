8df7560fdc99d4e100680657df97493a9b1df703
Stefan Norinder <stefan.norinder@sh.se>
2025-01-21 10:29:22 +0100

    indicate that the account is verified in Canvas

--- CanvasAccountRegistration.Logic/Model/AccountModelExtended.cs
+++ CanvasAccountRegistration.Logic/Model/AccountModelExtended.cs
@@ -6,10 +6,10 @@ namespace CanvasAccountRegistration.Logic.Model
     public partial class Account 
     {
         public string[] GetAssuranceLevels() => AssuranceLevel?.Replace(" ", "").Split(',') ?? Array.Empty<string>();
-        public virtual string GetSwamidAssuranceLevel() => string.Join(",", GetAssuranceLevels()
+        public virtual string GetSwamidAssuranceLevel() => string.Join(", ", GetAssuranceLevels()
               .Where(x => x.StartsWith("http://www.swamid.se/policy/assurance/"))?
               .Select(x => x.Substring("http://www.swamid.se/policy/assurance/".Length)));
-        public virtual string GetIdentityAssuranceProfile() => string.Join(",", GetAssuranceLevels()
+        public virtual string GetIdentityAssuranceProfile() => string.Join(", ", GetAssuranceLevels()
                 .Where(x => x.StartsWith("https://refeds.org/assurance/IAP/"))?
                 .Select(x => x.Substring("https://refeds.org/assurance/IAP/".Length)));
         public bool GetIsVerifiedWithId() => GetSwamidAssuranceLevel().Contains("al2") && (GetIdentityAssuranceProfile().Contains("medium") || GetIdentityAssuranceProfile().Contains("high"));
@@ -17,6 +17,6 @@ namespace CanvasAccountRegistration.Logic.Model
         public bool GetIsIntegrated() => IntegratedOn != null;
         public string GetAsSortableName() => $"{Surname}, {GivenName}";
         public string GetFullName() => $"{GivenName} {Surname}";
-        public string GetFullNameWithSmobPostfix() => $"{DisplayName} (smob)";
+        public string GetFullNameWithSmobPostfix() => $"{DisplayName} (verified-guest-account)";
     }
 } 
